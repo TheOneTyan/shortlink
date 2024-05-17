4f94f53207d09ccd4faa87a771069f7343083bb2
Sigman <sgapmx@gmail.com>
2024-05-17 17:49:00 +0800

    feature: 开发手机号脱敏功能

--- admin/src/main/java/org/cloud/shortlink/admin/dto/resp/UserRespDTO.java
+++ admin/src/main/java/org/cloud/shortlink/admin/dto/resp/UserRespDTO.java
@@ -1,6 +1,8 @@
 package org.cloud.shortlink.admin.dto.resp;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
+import org.cloud.shortlink.admin.convention.serialize.PhoneDesensitizationSerializer;
 
 /**
  * 用户返回参数响应
@@ -25,6 +27,7 @@ public class UserRespDTO {
     /**
      * 手机号
      */
+    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
     private String phone;
 
     /**
