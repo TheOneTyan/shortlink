5d5f755e6e757cc13092339f017aa4cb7a809ac2
Sigman <sgapmx@gmail.com>
2024-05-21 12:15:48 +0800

    fix: 修改能收到请求但返回404的错误

--- admin/src/main/java/org/cloud/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/org/cloud/shortlink/admin/controller/GroupController.java
@@ -2,13 +2,23 @@ package org.cloud.shortlink.admin.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.cloud.shortlink.admin.convention.result.Result;
+import org.cloud.shortlink.admin.convention.result.Results;
+import org.cloud.shortlink.admin.dto.req.ShortLinkGroupSaveReqDTO;
 import org.cloud.shortlink.admin.service.GroupService;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class GroupController {
 
     private final GroupService groupService;
 
+    @PostMapping("/api/short-link/v1/group")
+    public Result<Void> save(@RequestBody ShortLinkGroupSaveReqDTO requestParam){
+        groupService.saveGroup(requestParam);
+        return Results.success();
+    }
 }
