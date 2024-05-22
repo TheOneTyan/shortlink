bbc3bc094eacae44a44835a7c71634c6c0764480
Sigman <sgapmx@gmail.com>
2024-05-22 10:00:24 +0800

    feature: 新增创建短链接功能

---
+++ project/src/main/java/org/cloud/shortlink/project/controller/ShortLinkController.java
@@ -0,0 +1,27 @@
+package org.cloud.shortlink.project.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.cloud.shortlink.project.convention.result.Result;
+import org.cloud.shortlink.project.convention.result.Results;
+import org.cloud.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import org.cloud.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+import org.cloud.shortlink.project.service.ShortLinkService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ShortLinkController {
+
+    private final ShortLinkService shortLinkService;
+
+    /**
+     * 创建短链接
+     */
+    @PostMapping("/api/short-link/v1/link")
+    public Result<ShortLinkCreateRespDTO> save(@RequestBody ShortLinkCreateReqDTO requestParam){
+        return Results.success(shortLinkService.saveLink(requestParam));
+    }
+
+}
