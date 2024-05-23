ef913c683137e58a0e131f3c5cfccec3a1fa3808
Sigman <sgapmx@gmail.com>
2024-05-23 16:48:34 +0800

    feature: 新增路由表分表

--- admin/src/test/java/ShardingSphereTableSQLGenerator.java
+++ admin/src/test/java/ShardingSphereTableSQLGenerator.java
@@ -55,9 +55,19 @@ public class ShardingSphereTableSQLGenerator {
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
             """;
 
+    public static final String linkGoToTable =
+            """
+            CREATE TABLE `t_link_goto_%d` (
+              `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+              `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识',
+              `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+            """;
+
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
-            System.out.printf((groupTable) + "%n", i);
+            System.out.printf((linkGoToTable) + "%n", i);
         }
     }
 }
