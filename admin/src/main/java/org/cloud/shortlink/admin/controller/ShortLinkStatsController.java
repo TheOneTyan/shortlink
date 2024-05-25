1614bfc11d63658a1d0dad1953461727273aedfd
Sigman <sgapmx@gmail.com>
2024-05-25 18:14:12 +0800

    feature: 新增Admin获取短链接统计和Admin判断用户为新老用户功能

---
+++ admin/src/main/java/org/cloud/shortlink/admin/controller/ShortLinkStatsController.java
@@ -0,0 +1,41 @@
+package org.cloud.shortlink.admin.controller;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import lombok.RequiredArgsConstructor;
+import org.cloud.shortlink.admin.convention.result.Result;
+import org.cloud.shortlink.admin.dto.req.ShortLinkStatsAccessRecordReqDTO;
+import org.cloud.shortlink.admin.dto.req.ShortLinkStatsReqDTO;
+import org.cloud.shortlink.admin.dto.resp.ShortLinkStatsAccessRecordRespDTO;
+import org.cloud.shortlink.admin.dto.resp.ShortLinkStatsRespDTO;
+import org.cloud.shortlink.admin.remote.ShortLinkRemoteService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 短链接监控控制层
+ */
+@RestController
+@RequiredArgsConstructor
+public class ShortLinkStatsController {
+
+    /**
+     * 后续重构为 SpringCloud Feign 调用
+     */
+    ShortLinkRemoteService shortLinkRemoteService = new ShortLinkRemoteService() {};
+
+    /**
+     * 访问单个短链接指定时间内监控数据
+     */
+    @GetMapping("/api/short-link/admin/v1/stats")
+    public Result<ShortLinkStatsRespDTO> shortLinkStats(ShortLinkStatsReqDTO requestParam) {
+        return shortLinkRemoteService.oneShortLinkStats(requestParam);
+    }
+
+    /**
+     * 访问单个短链接指定时间内访问记录监控数据
+     */
+    @GetMapping("/api/short-link/admin/v1/stats/access-record")
+    public Result<IPage<ShortLinkStatsAccessRecordRespDTO>> shortLinkStatsAccessRecord(ShortLinkStatsAccessRecordReqDTO requestParam) {
+        return shortLinkRemoteService.shortLinkStatsAccessRecord(requestParam);
+    }
+}
--- admin/src/main/java/org/cloud/shortlink/admin/remote/ShortLinkRemoteService.java
+++ admin/src/main/java/org/cloud/shortlink/admin/remote/ShortLinkRemoteService.java
@@ -1,16 +1,18 @@
 package org.cloud.shortlink.admin.remote;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.TypeReference;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import org.cloud.shortlink.admin.convention.result.Result;
-import org.cloud.shortlink.admin.dto.req.RecycleBinClearReqDTO;
-import org.cloud.shortlink.admin.dto.req.RecycleBinMoveIntoReqDTO;
-import org.cloud.shortlink.admin.dto.req.RecycleBinRecoverReqDTO;
-import org.cloud.shortlink.admin.dto.req.ShortLinkRecycleBinPageReqDTO;
+import org.cloud.shortlink.admin.dto.req.*;
+import org.cloud.shortlink.admin.dto.resp.ShortLinkStatsAccessRecordRespDTO;
+import org.cloud.shortlink.admin.dto.resp.ShortLinkStatsRespDTO;
 import org.cloud.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import org.cloud.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import org.cloud.shortlink.admin.dto.req.ShortLinkStatsAccessRecordReqDTO;
+import org.cloud.shortlink.admin.dto.req.ShortLinkStatsReqDTO;
 import org.cloud.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import org.cloud.shortlink.admin.remote.dto.resp.ShortLinkGroupCountRespDTO;
 import org.cloud.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
@@ -63,5 +65,32 @@ public interface ShortLinkRemoteService {
         requestMap.put("size", requestParam.getSize());
         String resultJsonStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/recycle-bin/page", requestMap);
         return JSON.parseObject(resultJsonStr, new TypeReference<>() {});
+    }
+
+    /**
+     * 访问单个短链接指定时间内监控数据
+     *
+     * @param requestParam 访问短链接监控请求参数
+     * @return 短链接监控信息
+     */
+    default Result<ShortLinkStatsRespDTO> oneShortLinkStats(ShortLinkStatsReqDTO requestParam) {
+        String resultBodyStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/stats", BeanUtil.beanToMap(requestParam));
+        return JSON.parseObject(resultBodyStr, new TypeReference<>() {
+        });
+    }
+
+    /**
+     * 访问单个短链接指定时间内监控访问记录数据
+     *
+     * @param requestParam 访问短链接监控访问记录请求参数
+     * @return 短链接监控访问记录信息
+     */
+    default Result<IPage<ShortLinkStatsAccessRecordRespDTO>> shortLinkStatsAccessRecord(ShortLinkStatsAccessRecordReqDTO requestParam) {
+        Map<String, Object> stringObjectMap = BeanUtil.beanToMap(requestParam, false, true);
+        stringObjectMap.remove("orders");
+        stringObjectMap.remove("records");
+        String resultBodyStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/stats/access-record", stringObjectMap);
+        return JSON.parseObject(resultBodyStr, new TypeReference<>() {
+        });
     }
 }
