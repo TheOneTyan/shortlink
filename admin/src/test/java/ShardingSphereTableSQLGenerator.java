04c7b3f0038f65429350bc78df8d898e7c69deb4
Sigman <sgapmx@gmail.com>
2024-05-25 19:51:57 +0800

    fix: 修正t_link_stats_today的ShardingSphere分表列为gid

--- admin/src/test/java/ShardingSphereTableSQLGenerator.java
+++ admin/src/test/java/ShardingSphereTableSQLGenerator.java
@@ -82,7 +82,7 @@ public class ShardingSphereTableSQLGenerator {
               `update_time` datetime DEFAULT NULL COMMENT '修改时间',
               `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',
               PRIMARY KEY (`id`),
-              UNIQUE KEY `idx_unique_full-short-url` (`full_short_url`) USING BTREE
+              UNIQUE KEY `idx_unique_full-short-url` (`full_short_url`,`gid`,`date`) USING BTREE
             ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
             """;
 
