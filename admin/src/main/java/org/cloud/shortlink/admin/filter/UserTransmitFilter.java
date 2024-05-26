bb1ddf49307a3c4009a33b23461c8de9718dc273
Sigman <sgapmx@gmail.com>
2024-05-26 20:07:26 +0800

    fix: 修正用户登录无返回Token错误

--- admin/src/main/java/org/cloud/shortlink/admin/filter/UserTransmitFilter.java
+++ admin/src/main/java/org/cloud/shortlink/admin/filter/UserTransmitFilter.java
@@ -1,39 +1,64 @@
 package org.cloud.shortlink.admin.filter;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
+import com.google.common.collect.Lists;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.cloud.shortlink.admin.common.biz.user.UserContext;
 import org.cloud.shortlink.admin.common.biz.user.UserInfoDTO;
+import org.cloud.shortlink.admin.convention.exception.ClientException;
+import org.cloud.shortlink.admin.convention.result.Results;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
-import java.util.Objects;
+import java.io.PrintWriter;
+import java.util.List;
+
+import static org.cloud.shortlink.admin.common.enums.UserErrorCodeEnum.USER_TOKEN_FAIL;
 
 @RequiredArgsConstructor
 public class UserTransmitFilter implements Filter {
 
     private final StringRedisTemplate stringRedisTemplate;
+    private final List<String> IGNORE_URI = Lists.newArrayList(
+            "/api/short-link/admin/v1/user/login",
+            "/api/short-link/admin/v1/user/has-username",
+            "/api/short-link/admin/v1/user"
+    );
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-
-        // TODO 修改拦截路由
         String requestURI = httpServletRequest.getRequestURI();
-        if (Objects.equals(requestURI, "/api/short-link/admin/v1/user/login")) {
-            // TODO 登录功能不可用，会被拦截，想获取token可以把下一行取消注释
-//            filterChain.doFilter(servletRequest, servletResponse);
-            return;
-        }
-
-        String token = httpServletRequest.getHeader("token");
-        String username = httpServletRequest.getHeader("username");
-        String loginKey = "login_" + username;
-        Object userInfoStr = stringRedisTemplate.opsForHash().get(loginKey, token);
-        if (userInfoStr != null) {
-            UserInfoDTO userInfoDTO = JSON.parseObject(userInfoStr.toString(), UserInfoDTO.class);
+        if (!IGNORE_URI.contains(requestURI)) {
+            String username = httpServletRequest.getHeader("username");
+            String token = httpServletRequest.getHeader("token");
+            if (!StrUtil.isAllNotBlank(username, token)) {
+                try {
+                    returnJson((HttpServletResponse) servletResponse, JSON.toJSONString(Results.failure(new ClientException(USER_TOKEN_FAIL))));
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+                return;
+            }
+            Object userInfoJsonStr;
+            try {
+                userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+                if (userInfoJsonStr == null) {
+                    throw new ClientException(USER_TOKEN_FAIL);
+                }
+            } catch (Exception ex) {
+                try {
+                    returnJson((HttpServletResponse) servletResponse, JSON.toJSONString(Results.failure(new ClientException(USER_TOKEN_FAIL))));
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+                return;
+            }
+            UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
             // userInfoStr中没有token字段，所以需要手动给UserInfoDTO设置token
             userInfoDTO.setToken(token);
             UserContext.setUser(userInfoDTO);
@@ -43,6 +68,22 @@ public class UserTransmitFilter implements Filter {
             filterChain.doFilter(servletRequest, servletResponse);
         } finally {
             UserContext.removeUser();
+        }
+    }
+
+    private void returnJson(HttpServletResponse response, String json) throws Exception {
+        PrintWriter writer = null;
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=utf-8");
+        try {
+            writer = response.getWriter();
+            writer.print(json);
+
+        } catch (IOException e) {
+
+        } finally {
+            if (writer != null)
+                writer.close();
         }
     }
 }
